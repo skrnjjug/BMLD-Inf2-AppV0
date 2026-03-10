d8f11fe13583b8d9bec0e25419a771d50fc8d2c8
Lorna Skrnjug <skrnjuglorna@gmail.com>
2026-03-10 18:09:23 +0100

    username + co eingefügt

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+username = st.secrets["webdav.username"]
+password = st.secrets["webdav.password"]
+
+st.write(f"Username: {username}")
+st.write(f"Password: {password}")
+
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
